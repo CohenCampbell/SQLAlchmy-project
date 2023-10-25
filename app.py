1f65eab569dbf838db3be74487b417c1704dbbda
someguy <someguy@TheSuperUltraOmegaPhoneWavePremium>
2023-10-25 12:05:18 -0400

    commiting finished project, need to practice testing more

--- app.py
+++ app.py
@@ -1,12 +1,69 @@
 """Blogly application."""
-
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template, redirect, request
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = False
 
 connect_db(app)
+
+
 db.create_all()
+
+
+@app.route("/", methods=["POST", "GET"])
+def root():
+    edit = request.form.get("edit", False)
+    first = request.form.get("first", None)
+    last = request.form.get("last", None)
+    url = request.form.get("url", None)
+    if(url == ""):
+        url = 'https://braverplayers.org/wp-content/uploads/2022/09/blank-pfp.png'
+
+    if(first == None):
+        return redirect("users")
+    
+    if(edit == "True"):
+        userid = request.form.get("userData")
+        user = User.get_by_id(userid)
+        user.first_name = first
+        user.last_name = last
+        user.image_url = url
+        db.session.commit()
+        edit = False
+        return redirect("/users")
+    
+
+    new_user = User(first_name=first, last_name=last, image_url=url)
+    
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return redirect("/users")
+
+@app.route("/users")
+def users():
+    users = User.query.all()
+    return render_template("users.html", users=users)
+
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
+@app.route("/detail/<int:id>")
+def details(id):
+    user = User.query.get(id)
+    return render_template("detail.html", user=user)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    User.delete_user_byid(id)
+    return redirect("/users")
+
+@app.route("/edit<int:id>")
+def edit(id):
+    user = User.query.get(id)
+    return render_template("edit.html", user=user)
--- models.py
+++ models.py
@@ -1,1 +1,44 @@
-"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+    app.app_context().push()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.get(id)
+    
+    @classmethod
+    def delete_user_byid(cls, id):
+        user_del = cls.query.get(id)
+        db.session.delete(user_del)
+        db.session.commit()
+        return
+    
+    @classmethod
+    def get_full_name(cls, first, last):
+        return cls.query.filter(cls.first_name == first, cls.last_name == last).all()
+
+    id = db.Column(db.Integer,
+               primary_key=True,
+               autoincrement=True
+               )
+
+    first_name = db.Column(db.Text,
+                           nullable=False,
+                           unique=True)
+    
+    last_name = db.Column(db.Text,
+                           nullable=False,
+                           unique=True)
+    
+    image_url = db.Column(db.Text, 
+                          default='https://braverplayers.org/wp-content/uploads/2022/09/blank-pfp.png',
+                          nullable=False
+                          )
